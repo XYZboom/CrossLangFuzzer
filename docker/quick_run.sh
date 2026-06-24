c2861fa6a743d1af1ff72a9a745a7abad21d047f
xyzboom <xyzboom@qq.com>
2026-06-24 10:28:07 +0800

    feat: fix env var in docker

--- docker/quick_run.sh
+++ docker/quick_run.sh
@@ -3,6 +3,6 @@
 export JDK_1_8=/usr/lib/jvm/zulu-8-amd64/
 export JDK_17_0=/usr/lib/jvm/zulu-17-amd64/
 cd CrossLangFuzzer
-./gradlew :runner:kotlin-runner:run --args="-s"
+./gradlew :runners:kotlin-runner:run --args="-s"
 
 echo "See bugs at CrossLangFuzzer/out/min"
